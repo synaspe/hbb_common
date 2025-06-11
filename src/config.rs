da885644f369dd2a0d7cf542a17ef75f4812539a
synaspe <74139377+synaspe@users.noreply.github.com>
2025-06-11 20:37:24 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["atrust.sheng-jian.com"];
+pub const RS_PUB_KEY: &str = "bmNZxWD3gd2wDesaReRxLkCenA+WVkPcri0TG4IsrEA=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
